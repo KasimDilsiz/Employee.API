d21722b0c7dc81c4a3b71bb7a2fb4a55070804e2
KasimDilsiz <ksmdlsz@gmail.com>
2019-09-07 20:08:55 +0300

    employeeRoot api address created

--- apiRoots/employeeRoot.go
+++ apiRoots/employeeRoot.go
@@ -8,13 +8,13 @@ func EmployeeAPI(api *gin.RouterGroup) {
 	// CreateReadUpdateDelete = CRUD
 
 	// Create employee model           (CREATE)
-	api.POST("/employeePOST")
+	api.POST("/employeePOST")         // localhost:1111/api//employeePOST
 	// Read employee model             (READ)
-	api.GET("/employeesGET")
+	api.GET("/employeesGET")          // localhost:1111/api//employeeGET
 	// Read id option employee model   (READ)
-	api.GET("/employeesGET:id")
+	api.GET("/employeesGET:id")       // localhost:1111/api//employeeGET:id
 	// Update employee model           (UPDATE)
-	api.PUT("/employeeUpdate")
+	api.PUT("/employeeUpdate")        // localhost:1111/api//employeeUpdate
 	// Delete employee model           (DELETE)
-	api.DELETE("/employeeDELETE")
+	api.DELETE("/employeeDELETE")     // localhost:1111/api//employeeDELETE
 }
