994daf74611e691cc80ead495d96f7134d053acd
KasimDilsiz <ksmdlsz@gmail.com>
2019-09-07 19:11:43 +0300

    model created

--- models/employees.go
+++ models/employees.go
@@ -1,1 +1,11 @@
 package models
+
+import "time"
+
+type Employee struct {
+	ID int `json:"id gorm:"primary_key"`
+	EmployeeName string `json:"employee_name"`
+	EmployeeSalary int `json:"employee_salary"`
+	EmployeeAge uint8 `json:"employee_age"`
+	CreatedDate time.Time `json:"created_date"`
+}
